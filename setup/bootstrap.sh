c69faf393e6e606bd4284b3db506d784e8a84565
Smitty <me@smitop.com>
2022-03-05 11:54:21 -0500

    rm update check

--- setup/bootstrap.sh
+++ setup/bootstrap.sh
@@ -68,15 +68,15 @@ fi
 cd $HOME/mailinabox
 
 # Update it.
-if [ "$TAG" != $(git describe) ]; then
-	echo Updating Mail-in-a-Box to $TAG . . .
-	git fetch --depth 1 --force --prune origin tag $TAG
-	if ! git checkout -q $TAG; then
-		echo "Update failed. Did you modify something in $(pwd)?"
-		exit 1
-	fi
-	echo
-fi
+#if [ "$TAG" != $(git describe) ]; then
+#	echo Updating Mail-in-a-Box to $TAG . . .
+#	git fetch --depth 1 --force --prune origin tag $TAG
+#	if ! git checkout -q $TAG; then
+#		echo "Update failed. Did you modify something in $(pwd)?"
+#		exit 1
+#	fi
+#	echo
+#fi
 
 # Start setup script.
 setup/start.sh
